cdc8bac01a4992e3a421918c366ed046183ed706
root <root@us-dial-by-name>
2021-09-27 12:14:53 -0400

    changed git add script to not add dbntool folder bc its tracked in a separate repo

--- scripts/git_add.sh
+++ scripts/git_add.sh
@@ -2,16 +2,17 @@
 
 #adds new and modified files to git stage for commit
 
-git add -A /etc/dbntool/
-git add -u /etc/dbntool/
+#git add -A /etc/dbntool/
+#git add -u /etc/dbntool/
 git add -A /var/lib/asterisk/CCdbn/
 git add -u /var/lib/asterisk/CCdbn/
 git add -u /etc/asterisk/dbn_voicemail.conf
 git add -u /etc/asterisk/dbn_extensions.conf
 
+#removed from echo
+#git add -A /etc/dbntool/
+#git add -u /etc/dbntool/
 echo "
-git add -A /etc/dbntool/
-git add -u /etc/dbntool/
 git add -A /var/lib/asterisk/CCdbn/
 git add -u /var/lib/asterisk/CCdbn/
 git add -u /etc/asterisk/dbn_voicemail.conf
