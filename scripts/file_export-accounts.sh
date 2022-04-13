f8b0270eee6733e3ab53160f65869bcd8eef4fea
chris.bright <chris.bright@cloudcall.com>
2022-04-13 11:18:07 -0400

    changed printout to get the current version of the config, which persists when using the link. did this by setting up rsync on a new html directory that copies all accounts.csv files, then directed export script to the new directory

--- scripts/file_export-accounts.sh
+++ scripts/file_export-accounts.sh
@@ -10,14 +10,29 @@ fi
 cd /var/lib/asterisk/CCdbn/$custContext/
 #removing accounts file backup, using git instead
 #cp -p accounts.csv _accounts.csv
-cat _accounts.csv | grep -n '' | sed -E 's/:[0-9]+//g' > accounts.csv
+#cat _accounts.csv | grep -n '' | sed -E 's/:[0-9]+//g' > accounts.csv
+cat accounts.csv | grep -n '' | sed -E 's/:[0-9]+//g' > /tmp/dbntool-reorder
+cp /tmp/dbntool-reorder accounts.csv
+
+#old ad-hoc method
+#cp /var/lib/asterisk/CCdbn/$custContext/accounts.csv /home/chris.bright/htmlchris/"$custContext"-dbn.$(date +"%Y%m%d").csv
+#chmod 644 /home/chris.bright/htmlchris/"$custContext"-dbn.$(date +"%Y%m%d").csv
+
+#echo "$custContext/accounts.csv copied to htmlchris/ as "$custContext"-dbn.$(date +"%Y%m%d").csv"
+#echo
+#echo "$custContext Accounts available via http link below (CCVPN must be on):
+#http://172.31.36.3/chris/$custContext"-dbn.$(date +"%Y%m%d").csv""
+#echo
 
 
-cp /var/lib/asterisk/CCdbn/$custContext/accounts.csv /home/chris.bright/htmlchris/"$custContext"-dbn.$(date +"%Y%m%d").csv
-chmod 644 /home/chris.bright/htmlchris/"$custContext"-dbn.$(date +"%Y%m%d").csv
-echo "$custContext/accounts.csv copied to htmlchris/ as "$custContext"-dbn.$(date +"%Y%m%d").csv"
+#new daily update
+cp /var/lib/asterisk/CCdbn/$custContext/accounts.csv /var/www/html/ccdbn/$custContext/accounts.csv
+
+echo "$custContext/accounts.csv copied to /var/www/html/ccdbn/$custContext/accounts.csv"
 echo
 echo "$custContext Accounts available via http link below (CCVPN must be on):
-http://172.31.36.3/chris/$custContext"-dbn.$(date +"%Y%m%d").csv""
+http://172.31.36.3/ccdbn/$custContext/accounts.csv"
 echo
+
+
 dbntool show get-printout "$custContext"
